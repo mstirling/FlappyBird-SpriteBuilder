d6bf004427cf569f63a7a4728ccec677eeb5ac8a
mstirling <bstirling@pnu.edu.sa>
2014-10-27 18:47:54 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -14,7 +14,7 @@ character = (Character*)[CCBReader load:@"Character"];
 }
 
 // put new methods here
-- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event 
     // this will get called every time the player touches the screen
 }
   [character flap];
