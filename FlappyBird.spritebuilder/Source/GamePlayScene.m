b16f94b24a995cbead8563eb88fcf065163f9a8b
mstirling <bstirling@pnu.edu.sa>
2014-10-27 18:24:05 +0300

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -6,8 +6,8 @@
 
 - (void)initialize
 {
-    // your code here
-}character = (Character*)[CCBReader load:@"Character"];[physicsNode addChild:character];
+    // your code here}
+[character = (Character*)[CCBReader load:@"Character"];[physicsNode addChild:character];
 
 -(void)update:(CCTime)delta
 {
@@ -17,6 +17,7 @@
 // put new methods here
 - (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event {
     // this will get called every time the player touches the screen
-}[character flap];
+}
+  [character flap];
 @end
 
